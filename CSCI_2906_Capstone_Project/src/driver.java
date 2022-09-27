2bd140de26f10b2406ccfdec4570a2a4b45ce5f3
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-09-27 15:37:27 -0600

    Add Driver that captures sound

---
+++ CSCI_2906_Capstone_Project/src/driver.java
@@ -0,0 +1,97 @@
+import javax.sound.sampled.*;
+import java.io.*;
+
+public class driver {
+    // record duration, in milliseconds
+    static final long RECORD_TIME = 6000;  // 1 minute = 60000
+
+    // path of the wav file
+    File wavFile = new File("C:\\Users\\dalle\\Downloads\\trialAudio2.wav");
+
+    // format of audio file
+    AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+
+    // the line from which audio data is captured
+    TargetDataLine line;
+
+    /**
+     * Defines an audio format
+     */
+    AudioFormat getAudioFormat() {
+        float sampleRate = 16000;
+        int sampleSizeInBits = 8;
+        int channels = 2;
+        boolean signed = true;
+        boolean bigEndian = true;
+        return new AudioFormat(sampleRate, sampleSizeInBits,
+                channels, signed, bigEndian);
+    }
+
+    /**
+     * Captures the sound and record into a WAV file
+     */
+    void start() {
+        try {
+            AudioFormat format = getAudioFormat();
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+
+            // checks if system supports the data line
+            if (!AudioSystem.isLineSupported(info)) {
+                System.out.println("Line not supported");
+                System.exit(0);
+            }
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+            line.start();   // start capturing
+
+            System.out.println("Start capturing...");
+
+            AudioInputStream ais = new AudioInputStream(line);
+
+            System.out.println("Start recording...");
+
+            // start recording
+            AudioSystem.write(ais, fileType, wavFile);
+
+        } catch (LineUnavailableException ex) {
+            ex.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    /**
+     * Closes the target data line to finish capturing and recording
+     */
+    void finish() {
+        line.stop();
+        line.close();
+        System.out.println("Finished");
+    }
+
+    /**
+     * Entry to run the program
+     */
+    public static void main(String[] args) {
+        final driver recorder = new driver();
+
+        // creates a new thread that waits for a specified
+        // of time before stopping
+        Thread stopper = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(RECORD_TIME);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                recorder.finish();
+            }
+        });
+
+        stopper.start();
+
+        // start recording
+        recorder.start();
+    }
+
+}
