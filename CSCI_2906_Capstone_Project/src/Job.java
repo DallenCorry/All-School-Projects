671f94fd17302edefb6c68b07f714becbb167a6a
Dallen Corry <dallenrobert@gmail.com>
2022-10-06 11:43:36 -0600

    Created Job and User classes. Removed old driver.

---
+++ CSCI_2906_Capstone_Project/src/Job.java
@@ -0,0 +1,169 @@
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+
+public class Job {
+    int jobID;
+    User creator;
+    String title;
+    Date datePosted;
+    Date dateOfJob;
+    double pay;
+    boolean payIsHourly;
+    double jobTime;
+    String description;
+    String location;
+    int numWorkersWanted;
+    String category;
+    boolean isActive;
+    ArrayList<User> workersApplied;
+    Image jobImage;
+
+    public Job(User creator, String title, Date dateOfJob, double pay, boolean payIsHourly, String location) {
+        this.creator = creator;
+        this.title = title;
+        this.dateOfJob = dateOfJob;
+        this.pay = pay;
+        this.payIsHourly = payIsHourly;
+        this.location = location;
+        jobID = getUniqueID();
+    }
+
+    public Job(User creator, String title, Date dateOfJob, double pay, boolean payIsHourly, double jobTime, String description, String location, int numWorkersWanted, String category) {
+        this.creator = creator;
+        this.title = title;
+        this.dateOfJob = dateOfJob;
+        this.pay = pay;
+        this.payIsHourly = payIsHourly;
+        this.jobTime = jobTime;
+        this.description = description;
+        this.location = location;
+        this.numWorkersWanted = numWorkersWanted;
+        this.category = category;
+        jobID = getUniqueID();
+    }
+
+    public int getJobID() {
+        return jobID;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+    public Date getDateOfJob() {
+        return dateOfJob;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public boolean isPayIsHourly() {
+        return payIsHourly;
+    }
+
+    public double getJobTime() {
+        return jobTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getNumWorkersWanted() {
+        return numWorkersWanted;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public ArrayList<User> getWorkersApplied() {
+        return workersApplied;
+    }
+
+    public Image getJobImage() {
+        return jobImage;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDateOfJob(Date dateOfJob) {
+        this.dateOfJob = dateOfJob;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    public void setPayIsHourly(boolean payIsHourly) {
+        this.payIsHourly = payIsHourly;
+    }
+
+    public void setJobTime(double jobTime) {
+        this.jobTime = jobTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setNumWorkersWanted(int numWorkersWanted) {
+        this.numWorkersWanted = numWorkersWanted;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setJobImage(Image jobImage) {
+        this.jobImage = jobImage;
+    }
+
+    @Override
+    public String toString() {
+        if(payIsHourly) {
+            return title + "\nCreated by: " + creator + "\n$" + pay+"/hr, "+jobTime;
+        } else {
+            return title + "\nCreated by: " + creator + "\n$" + pay;
+        }
+    }
+
+    static String encode(Job job) {
+        return null;
+    }
+
+    static Job decode(String string) {
+        return null;
+    }
+
+    public static int getUniqueID() {
+        Random r = new Random();
+        return r.nextInt(1000);
+    }
+}
---
+++ CSCI_2906_Capstone_Project/src/User.java
@@ -0,0 +1,148 @@
+import javafx.scene.image.Image;
+
+import java.time.Period;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class User {
+    int userID;
+    String name;
+    private String userName;
+    private String password;
+    private LocalDate DOB;
+    private String gender;
+    ArrayList<String> skills;
+    double employerRating;
+    double workerRating;
+    Image profilePicture;
+    private ArrayList<Job> jobsWorked;
+    private ArrayList<Job> jobsCreated;
+    private double earnings;
+    private double earningGoal;
+
+    //Constructors
+    public User(String name, String userName, String password, LocalDate DOB) {
+        this.name = name;
+        this.userName = userName;
+        this.password = password;
+        this.DOB = DOB;
+        userID = getUniqueID();
+    }
+
+    // Getters and Setters
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDate getDOB() {
+        return DOB;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public ArrayList<String> getSkills() {
+        return skills;
+    }
+
+    public double getEmployerRating() {
+        return employerRating;
+    }
+
+    public double getWorkerRating() {
+        return workerRating;
+    }
+
+    public Image getProfilePicture() {
+        return profilePicture;
+    }
+
+    public ArrayList<Job> getJobsWorked() {
+        return jobsWorked;
+    }
+
+    public ArrayList<Job> getJobsCreated() {
+        return jobsCreated;
+    }
+
+    public double getEarnings() {
+        return earnings;
+    }
+
+    public double getEarningGoal() {
+        return earningGoal;
+    }
+
+    public int getAge() {
+        return Period.between(DOB, LocalDate.now()).getYears();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setProfilePicture(Image profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public void setEarningGoal(double earningGoal) {
+        this.earningGoal = earningGoal;
+    }
+
+    public void updateRatings() {
+        employerRating = avgRating(this, "employer");
+        workerRating = avgRating(this, "worker");
+    }
+
+    public void addSkill(String newSkill) {
+        skills.add(newSkill);
+    }
+
+    public void addJobWorked(Job newJob) {
+        jobsWorked.add(newJob);
+    }
+
+    public void addJobCreated(Job newJob) {
+        jobsCreated.add(newJob);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: "+name+"\nAge: "+getAge()+"\nGender: "+gender;
+    }
+
+    //TODO: calculate an average rating based on reviews.
+    public static double avgRating(User user, String type) {
+        double avg;
+        if (type.equals("employer")) {
+            avg = user.getJobsCreated().size()/5;
+        } else {
+            avg = user.getJobsWorked().size()/5;
+        }
+        return avg;
+    }
+
+    //TODO: Create unique IDs
+    public static int getUniqueID() {
+        Random r = new Random();
+        return r.nextInt(1000);
+    }
+}
