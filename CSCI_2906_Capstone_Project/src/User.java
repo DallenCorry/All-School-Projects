79f87f0692b52a0c1360fb6ffbfa33e9f4965597
Dallen Corry <89269364+DallenCorry@users.noreply.github.com>
2022-11-01 11:52:20 -0600

    Make encode work, sort of do write to database
    check out writeUTF, and see if we can get that to actually function correctly.

--- CSCI_2906_Capstone_Project/src/User.java
+++ CSCI_2906_Capstone_Project/src/User.java
@@ -1,4 +1,4 @@
-package com.example.oddjob;
+package sample;
 /**
  * @author: Dallen Corry
  * @version: 1.0
@@ -6,12 +6,17 @@ package com.example.oddjob;
  * @created: 2022/Oct/06
  * Class: User
  * */
-import javafx.scene.image.Image;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.net.URL;
 import java.time.Period;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class User {
     int userID;
@@ -24,11 +29,15 @@ public class User {
     ArrayList<String> skills;
     double employerRating;
     double workerRating;
-    Image profilePicture;
+    String profilePicturePath;
     private ArrayList<Job> jobsWorked;
     private ArrayList<Job> jobsCreated;
     private double earnings;
     private double earningGoal;
+
+    private final URL URL_TO_USER_DATA = getClass().getResource("users.txt");
+    private final static char ENCODED_DATA_SEPARATOR = '<';
+
 
     //Constructors
     public User() {
@@ -36,14 +45,31 @@ public class User {
         userName = null;
         password=null;
         DOB=null;
-        userID = getUniqueID();
+        userID = generateUniqueID();
     }
     public User(String name, String userName, String password, LocalDate DOB) {
         this.name = name;
         this.userName = userName;
         this.password = password;
         this.DOB = DOB;
-        userID = getUniqueID();
+        userID = generateUniqueID();
+    }
+
+    /**
+     * String[] data in the following format:
+     *      name, DOB, gender, email, userName, password.
+     *
+     */
+    public User(String[] data) {
+        name = data[0];
+        DOB = LocalDate.parse(data[1]);
+        gender = data[2];
+        email = data[3];
+        userName = data[4];
+        password = data[5];
+
+        userID = generateUniqueID();
+
     }
 
     // Getters and Setters
@@ -87,8 +113,8 @@ public class User {
         return workerRating;
     }
 
-    public Image getProfilePicture() {
-        return profilePicture;
+    public String getProfilePicturePath() {
+        return profilePicturePath;
     }
 
     public ArrayList<Job> getJobsWorked() {
@@ -126,8 +152,8 @@ public class User {
         this.email = email;
     }
 
-    public void setProfilePicture(Image profilePicture) {
-        this.profilePicture = profilePicture;
+    public void setProfilePicturePath(String profilePicturePath) {
+        this.profilePicturePath = profilePicturePath;
     }
 
     public void setEarningGoal(double earningGoal) {
@@ -168,8 +194,111 @@ public class User {
     }
 
     //TODO: Create unique IDs
-    public static int getUniqueID() {
+    private static int generateUniqueID() {
         Random r = new Random();
         return r.nextInt(1000);
+    }
+
+    public boolean addToDatabase() {
+        try {
+//            System.out.println(URL_TO_USER_DATA);
+//            System.out.println(URL_TO_USER_DATA.getPath());
+            RandomAccessFile raf = new RandomAccessFile("users.txt", "rw");//URL_TO_USER_DATA.getPath()
+//            raf.seek(raf.length());//put it at the end of the file
+            raf.seek(0);
+            raf.writeUTF("1000");
+            raf.writeUTF("2222");
+//            System.out.println(raf.readInt());
+            raf.close();
+
+//            PrintWriter userWrite = new PrintWriter(f);
+
+//            String s = encode();
+////            System.out.println(s);
+//            userWrite.write(s);
+////            userWrite.println("HELLO!");
+////            userWrite.print("Text 2");
+////            userWrite.println("HELLO!");
+//            userWrite.close();
+//
+//            Scanner userScan = new Scanner(f);
+//            System.out.println("First: "+userScan.nextLine());
+//            System.out.println("Second"+userScan.nextLine());
+//
+//            userScan.close();
+        } catch(FileNotFoundException e) {
+            System.out.println("Error:" + e.getMessage());
+            return false;
+        } catch (Exception e) {
+            System.out.println("Experienced unhandled error: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    private String[] getAllData() {
+        ArrayList<String> arrLst = new ArrayList<>();
+
+        arrLst.add(userID+"");
+        arrLst.add(name);
+        arrLst.add(userName);
+        arrLst.add(password);
+        arrLst.add(DOB.toString());
+        arrLst.add(gender);
+        arrLst.add(email);
+
+        arrLst.add(employerRating+"");
+        arrLst.add(workerRating+"");
+        arrLst.add(earnings+"");
+        arrLst.add(earningGoal+"");
+
+        arrLst.add(profilePicturePath);
+
+        if(skills != null && !skills.isEmpty()) {
+            arrLst.add("SKILLS");
+            for (String s : skills) {
+                arrLst.add(s);
+            }
+        }
+        if(jobsWorked != null && !jobsWorked.isEmpty()) {
+            arrLst.add("JOBS_WORKED");
+            for (Job j : jobsWorked) {
+                arrLst.add("JobID:" + j.getJobImage());
+            }
+        }
+        if(jobsCreated != null && !jobsCreated.isEmpty()) {
+            arrLst.add("JOBS_CREATED");
+            for (Job j : jobsCreated) {
+                arrLst.add("JobID:" + j.getJobImage());
+            }
+        }
+
+        String[] arr = new String[arrLst.size()];
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = arrLst.get(i);
+        }
+        return arr;
+    }
+
+    public String encode() {
+        return encode(this);
+    }
+
+    public static String encode(User u) {
+
+        StringBuilder s = new StringBuilder();
+
+
+        String[] arr = u.getAllData();
+        for (String str: arr) {
+            s.append(str).append(ENCODED_DATA_SEPARATOR);
+        }
+        return s.toString();
+    }
+
+    public static User decode(String string) {
+        return null;
     }
 }
