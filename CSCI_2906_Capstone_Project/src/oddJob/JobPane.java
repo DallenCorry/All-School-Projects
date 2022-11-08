994b44596f8b1133076bb966480b53eedf0ecf80
Dallen Corry <89269364+DallenCorry@users.noreply.github.com>
2022-11-08 11:50:23 -0700

    Jobs clicked now open new JobInfoPane

--- CSCI_2906_Capstone_Project/src/oddJob/JobPane.java
+++ CSCI_2906_Capstone_Project/src/oddJob/JobPane.java
@@ -6,21 +6,26 @@ package oddJob;
  * @created: 2022/Oct/10
  * Class: JobPane
  * */
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.InputEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.image.Image;
-import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import static oddJob.Defaults.*;
 
 public class JobPane extends Pane {
     Job job;
     TextArea t;
     Image img;
-    private static final int defaultWidth = 200;
-    private static final int defaultHeight = 100;
-    private static final Image defaultImage = new Image("images/imageNotFound.png");
+    private static final int jobWidth = 200;
+    private static final int jobHeight = 100;
+    private static final Image defaultImage = new Image(IMAGE_NOT_FOUND);
     JobPane() {
         super();
         job = null;
@@ -29,6 +34,7 @@ public class JobPane extends Pane {
         formatPane();
     }
 
+    //TODO fix make this work
     JobPane(Job job) {
         super();
         this.job=job;
@@ -40,13 +46,17 @@ public class JobPane extends Pane {
         }
         formatPane();
 //        getChildren().add(t);
+
+        jobClickedHandler x = new jobClickedHandler();
+        setOnMouseClicked(x);
+        t.setOnMouseClicked(x);
     }
 
     public void formatPane() {
         t.setLayoutY(1);
         t.setLayoutX(1);
-        t.setPrefWidth(defaultWidth);
-        t.setPrefHeight(defaultHeight);
+        t.setPrefWidth(jobWidth);
+        t.setPrefHeight(jobHeight);
         t.setWrapText(true);
         t.setEditable(false);
         HBox box = new HBox();
@@ -54,5 +64,15 @@ public class JobPane extends Pane {
         box.getChildren().addAll(view,t);
 
         getChildren().add(box);
+    }
+    class jobClickedHandler implements EventHandler<InputEvent> {
+        @Override
+        public void handle(InputEvent e) {
+            System.out.println("Clicked on a Job");
+            Scene scene = new Scene(new JobInfoPane(job));
+            Stage newStage = new Stage();
+            newStage.setScene(scene);
+            newStage.show();
+        }
     }
 }
