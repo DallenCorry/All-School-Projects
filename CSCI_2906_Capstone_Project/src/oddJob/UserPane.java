b43bce343b1e58bfec8a3aee962fa75229de19d3
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-11-10 10:32:57 -0700

    add placeholders for future controls

--- CSCI_2906_Capstone_Project/src/oddJob/UserPane.java
+++ CSCI_2906_Capstone_Project/src/oddJob/UserPane.java
@@ -1,11 +1,28 @@
 package oddJob;
 
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
 public class UserPane extends VBox {
-    UserPane() {
+    Text name = new Text();
+    Image profile;
+    Text jobsWorked = new Text();
+    Text jobsPosted = new Text();
+
+    Button btnMore = new Button("More");
+    VBox additionalInfo = new VBox();
+    Text userName = new Text();
+    Text userID = new Text();
+    Text age = new Text();
+    Text gender = new Text();
+    Text email = new Text();
+
+
+    UserPane(User u) {
         super();
+
         getChildren().add(new Text("Hello World!"));
     }
 }
